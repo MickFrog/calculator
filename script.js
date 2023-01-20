cbb1d448d35ea8464f89d60e09fe7644f7f8e2e0
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-01-20 12:22:51 +0300

    Add +-/* and operate Js functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return parseFloat((a+b).toFixed(1));
+}
+
+function subtract(a, b) {
+    return parseFloat((a-b).toFixed(1));
+}
+
+function multiply(a, b) {
+    return parseFloat((a*b).toFixed(1));
+}
+
+function divide(a, b) {
+    return parseFloat((a/b).toFixed(1));
+}
+
+function operate(operand, a, b) {
+    switch (operand) {
+        case "+":
+            return add(a, b);
+
+        case "-":
+            return subtract(a, b);
+
+        case "*":
+            return multiply(a, b);
+
+        case "/":
+            return divide(a, b);
+    }
+}
