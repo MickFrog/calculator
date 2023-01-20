773fdbb466dd4265ac1a49011c95041e59eae0ed
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-01-20 14:58:39 +0300

    In process, add operand display

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const numButtons = [...document.querySelectorAll(".num")];
 let lowerScreen = document.getElementById("downScreen");
 //get operand buttons
 const operandButtons = [...document.querySelectorAll(".operate")];
+//get upper screen
+let upperScreen = document.getElementById("upScreen");
+
+//global operation values
+let operand = "";
+let a = null;
+let b = null;
 
 function addNumDisplay(NumList) { //Adds display of number to screen
     NumList.forEach(element => {
@@ -14,15 +21,19 @@ function addNumDisplay(NumList) { //Adds display of number to screen
     });
 }
 
-// function addOperandDisplay(operateList) { //Adds display of operation symbols to screen
-//     operateList.forEach(element => {
-//         element.addEventListener('click', () => {
-//             if(lowerScreen.textContent.length >= 16) return;
+function addOperandDisplay(operateList) { //Adds display of operation symbols to screen
+    operateList.forEach(element => {
+        element.addEventListener('click', () => {
+            if(a == null) {
+                a = lowerScreen.textContent;
+                operand = element.textContent;
+                upperScreen.textContent += a;
+            }
 
-//             lowerScreen.textContent += element.textContent;
-//         })
-//     })
-// }
+            upperScreen.textContent += element.textContent;
+        })
+    })
+}
 
 function add(a, b) {
     return parseFloat((a+b).toFixed(1));
